ef91b1f8d805a03bde18dc98f2edb05df8e1a858
muon-Inc <64858564+muon-Inc@users.noreply.github.com>
2020-06-26 02:50:26 +0530

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+    $('#nav-icon1').on('click', function () {
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    });
+
+    $('.top-nav .nav-link').on('click', function () {
+        $('#nav-icon1').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+
+    $('nav a[href*="#"]').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 2000);
+    });
+
+    $('a[href*="#about"]').on('click', function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 2000);
+    });
+
+    $('#up').on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 2000);
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: false
+    });
+});
+
+$(window).ready(function () {
+
+    var wHeight = $(window).height();
+
+    $('.part')
+        // .height(wHeight)
+        .scrollie({
+            scrollOffset: -400,
+            scrollingInView: function (elem) {
+
+                var bgColor = elem.data('background');
+
+                $('body').css('background-color', bgColor);
+
+            }
+        });
+});
