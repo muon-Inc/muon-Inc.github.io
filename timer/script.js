1edb9bc6c6ba064eb3c96d47f07f7336e1a9bd83
muon-Inc <64858564+muon-Inc@users.noreply.github.com>
2020-09-13 21:52:49 +0530

    Add files via upload

--- timer/script.js
+++ timer/script.js
@@ -3,6 +3,7 @@ var elem = document.documentElement;
 
 function openFullScreen() {
   var icon = document.getElementById("icon");
+  document.getElementById("form-submit-btn").style.display = "none";
   if (icon.classList.contains("fa-expand-alt")) {
     icon.classList.remove("fa-expand-alt");
     icon.classList.add("fa-compress-alt");
@@ -21,6 +22,7 @@ function openFullScreen() {
   } else if (icon.classList.contains("fa-compress-alt")) {
     icon.classList.remove("fa-compress-alt");
     icon.classList.add("fa-expand-alt");
+    document.getElementById("form-submit-btn").style.display = "block";
     if (document.exitFullscreen) {
       document.exitFullscreen();
     } else if (document.mozCancelFullScreen) {
@@ -37,6 +39,86 @@ function openFullScreen() {
 
 }
 // 
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var radius = canvas.height / 2;
+ctx.translate(radius, radius);
+radius = radius * 0.90
+setInterval(drawClock, 1000);
+
+function drawClock() {
+  drawFace(ctx, radius);
+  drawNumbers(ctx, radius);
+  drawTime(ctx, radius);
+}
+
+function drawFace(ctx, radius) {
+  var grad;
+  ctx.beginPath();
+  ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+  ctx.fillStyle = '#dbe7ed';
+  ctx.fill();
+  grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
+  grad.addColorStop(0, '#333');
+  grad.addColorStop(0.5, '#96cdeb');
+  grad.addColorStop(1, '#96cdeb');
+  ctx.strokeStyle = grad;
+  ctx.lineWidth = radius * 0.1;
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
+  ctx.fillStyle = '#333';
+  ctx.fill();
+}
+
+function drawNumbers(ctx, radius) {
+  var ang;
+  var num;
+  ctx.font = radius * 0.15 + "px arial";
+  ctx.textBaseline = "middle";
+  ctx.textAlign = "center";
+  for (num = 1; num < 13; num++) {
+    ang = num * Math.PI / 6;
+    ctx.rotate(ang);
+    ctx.translate(0, -radius * 0.85);
+    ctx.rotate(-ang);
+    ctx.fillText(num.toString(), 0, 0);
+    ctx.rotate(ang);
+    ctx.translate(0, radius * 0.85);
+    ctx.rotate(-ang);
+  }
+}
+
+function drawTime(ctx, radius) {
+  var now = new Date();
+  var hour = now.getHours();
+  var minute = now.getMinutes();
+  var second = now.getSeconds();
+  //hour
+  hour = hour % 12;
+  hour = (hour * Math.PI / 6) +
+    (minute * Math.PI / (6 * 60)) +
+    (second * Math.PI / (360 * 60));
+  drawHand(ctx, hour, radius * 0.5, radius * 0.07);
+  //minute
+  minute = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60));
+  drawHand(ctx, minute, radius * 0.8, radius * 0.07);
+  // second
+  second = (second * Math.PI / 30);
+  drawHand(ctx, second, radius * 0.9, radius * 0.02);
+}
+
+function drawHand(ctx, pos, length, width) {
+  ctx.beginPath();
+  ctx.lineWidth = width;
+  ctx.lineCap = "round";
+  ctx.moveTo(0, 0);
+  ctx.rotate(pos);
+  ctx.lineTo(0, -length);
+  ctx.stroke();
+  ctx.rotate(-pos);
+}
 
 // Get real time 
 setInterval(function () {
@@ -77,7 +159,7 @@ setInterval(function () {
 
   var time = hh + ":" + min + ":" + sec + " " + ampm;
 
-  document.getElementById("currentTime").innerHTML = time;
+  // document.getElementById("currentTime").innerHTML = time;
   document.getElementById("currentDate").innerHTML = date;
 }, 100);
 
